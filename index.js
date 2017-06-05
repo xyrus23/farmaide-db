f073aa93bca2842af24b3361a4a19d7610c6c3e9
xyrus23 <xyrus.cruz23@gmail.com>
2017-06-05 10:15:40 +0800

    dynamic port

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/public'));
+
 // configure the app to use bodyParser()
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
-app.use(express.static('public'));
-var server = app.listen(8000, function(){
-    var port = server.address().port;
-    console.log('App is running at port %s', port);
-});
-
 //database
 app.use(require(__dirname + '/public/config/router')(express.Router()));
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+})
+
+// app.use(express.static('public'));
+// var server = app.listen(8000, function(){
+//     var port = server.address().port;
+//     console.log('App is running at port %s', port);
+// });
